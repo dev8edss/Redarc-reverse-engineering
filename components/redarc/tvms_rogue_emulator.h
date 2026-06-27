2db5176b0cec0154f62e667c0ae65e97bed920f4
dev8edss <77666361+dev8edss@users.noreply.github.com>
2026-06-27 23:44:38 +0800

    Move emulator header segments out of component root

--- components/redarc/tvms_rogue_emulator.h
+++ components/redarc/tvms_rogue_emulator.h
@@ -1,18 +1,18 @@
 #pragma once
 
-// Split into ordered include segments to keep each generated mapping section
-// independently reviewable. Concatenating these files reproduces the complete
-// TVMS Rogue emulator declaration.
-#include "tvms_rogue_emulator_header_00.h"
-#include "tvms_rogue_emulator_header_01.h"
-#include "tvms_rogue_emulator_header_02.h"
-#include "tvms_rogue_emulator_header_03a.h"
-#include "tvms_rogue_emulator_header_03b.h"
-#include "tvms_rogue_emulator_header_04.h"
-#include "tvms_rogue_emulator_header_05.h"
-#include "tvms_rogue_emulator_header_06.h"
-#include "tvms_rogue_emulator_header_07.h"
-#include "tvms_rogue_emulator_header_08.h"
-#include "tvms_rogue_emulator_header_09.h"
-#include "tvms_rogue_emulator_header_10.h"
-#include "tvms_rogue_emulator_header_11.h"
+// Split into ordered private header segments to keep each generated mapping
+// section independently reviewable. The segment headers live outside the
+// component root so ESPHome does not auto-include them individually.
+#include "tvms_rogue_emulator_segments/header_00.h"
+#include "tvms_rogue_emulator_segments/header_01.h"
+#include "tvms_rogue_emulator_segments/header_02.h"
+#include "tvms_rogue_emulator_segments/header_03a.h"
+#include "tvms_rogue_emulator_segments/header_03b.h"
+#include "tvms_rogue_emulator_segments/header_04.h"
+#include "tvms_rogue_emulator_segments/header_05.h"
+#include "tvms_rogue_emulator_segments/header_06.h"
+#include "tvms_rogue_emulator_segments/header_07.h"
+#include "tvms_rogue_emulator_segments/header_08.h"
+#include "tvms_rogue_emulator_segments/header_09.h"
+#include "tvms_rogue_emulator_segments/header_10.h"
+#include "tvms_rogue_emulator_segments/header_11.h"
